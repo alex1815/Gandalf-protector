bfac153dbf192e2d9174a7e4eef6439992d5db73
alekseykhorev <alex.xorev@gmail.com>
2020-02-23 12:33:47 +0300

    * Added integration test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,44 @@
 //@flow
 import React from "react";
-import { render } from "@testing-library/react";
+import {
+  fireEvent,
+  render,
+  queryByTestId,
+  getByTestId,
+  waitForElementToBeRemoved
+} from "@testing-library/react";
 import App from "./App";
+import { COME_IN_TEXT } from "./components/TryCatchMe/TryCatchMe";
+import { YOU_SHELL_NOT_PASS_DATA_TEST_ID } from "./components/YouShellNotPass/YouShellNotPass";
 
 test("Basic app render without crash", () => {
   const { container } = render(<App />);
   expect(container).toBeInTheDocument();
+});
+
+test("After trying to come in Gandalf protect the enter", () => {
+  const { getByText, container } = render(<App />);
+
+  const comeInElement = getByText(COME_IN_TEXT);
+  expect(queryByTestId(container, YOU_SHELL_NOT_PASS_DATA_TEST_ID)).toBeNull();
+
+  fireEvent.mouseEnter(comeInElement);
+  expect(
+    getByTestId(container, YOU_SHELL_NOT_PASS_DATA_TEST_ID)
+  ).toBeInTheDocument();
+});
+
+test("After trying to come and Gandalf protected the enter Gandalf go to rest", async () => {
+  const { getByText, container } = render(<App />);
+
+  const comeInElement = getByText(COME_IN_TEXT);
+  fireEvent.mouseEnter(comeInElement);
+
+  expect(
+    getByTestId(container, YOU_SHELL_NOT_PASS_DATA_TEST_ID)
+  ).toBeInTheDocument();
+  // Gandalf go to rest
+  await waitForElementToBeRemoved(() =>
+    queryByTestId(container, YOU_SHELL_NOT_PASS_DATA_TEST_ID)
+  );
 });
--- src/components/YouShellNotPass/YouShellNotPass.js
+++ src/components/YouShellNotPass/YouShellNotPass.js
@@ -5,6 +5,8 @@ import * as youShellNotPass from "../../assets/youShallNotPass.jpeg";
 import "./YouShellNotPass.css";
 
 const PROTECTION_TIME = 1;
+export const YOU_SHELL_NOT_PASS_DATA_TEST_ID =
+  "YOU_SHELL_NOT_PASS_DATA_TEST_ID";
 
 type YouShellNotPassProps = {
   somebodyTryToPassAmount: number
@@ -34,7 +36,10 @@ const YouShellNotPass = ({ somebodyTryToPassAmount }: YouShellNotPassProps) => {
   }
   // before image turned around like react app logo. It was really fun, but you can't come through the Gandalf.
   return (
-    <div className={"Gandalf-container"}>
+    <div
+      className={"Gandalf-container"}
+      data-testid={YOU_SHELL_NOT_PASS_DATA_TEST_ID}
+    >
       <div className={"Gandalf-image"}>
         {/*// can specify the size of picture if want*/}
         <img src={youShellNotPass} alt="Gandalf on guard" />
